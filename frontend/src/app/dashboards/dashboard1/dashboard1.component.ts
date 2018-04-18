733a9bfdfbd06599d9b8ed9f98376cbb6fb5a3e1
Kamran Ali <kamran_ali@hotmail.com>
2018-04-18 11:02:47 +0500

    added the refresh select box and 4th button

--- frontend/src/app/dashboards/dashboard1/dashboard1.component.ts
+++ frontend/src/app/dashboards/dashboard1/dashboard1.component.ts
@@ -9,22 +9,36 @@ import 'rxjs/add/operator/map';
 })
 export class Dashboard1Component {
 	rooms:any = [];
+	timeout:string = "1";
+	timeoutArray = ['1', '5', '60'];
 	constructor(public jsonp:Jsonp) {
 		/*let data = '[{"id":"367","rm_timestamp":"2018-04-17 02:38:15","rm_puerto_a":"247","rm_puerto_c":"255","rm_puerto_f":"221","rm_puerto_k":"255","rm_puerta1":"t","rm_puerta2":"t","rm_puerta3":"t","rm_puerta4":"f","rm_puerta5":"t","rm_puerta6":"t","rm_puerta7":"t","rm_puerta8":"t","rm_puerta9":"t","rm_puerta10":"t","rm_puerta11":"t","rm_puerta12":"t","rm_puerta13":"t","rm_puerta14":"t","rm_puerta15":"t","rm_puerta16":"t","rm_puerta17":"t","rm_puerta18":"f","rm_puerta19":"t","rm_puerta20":"t","rm_puerta21":"t","rm_puerta22":"f","rm_puerta23":"t","rm_puerta24":"t","rm_puerta25":"t","rm_puerta26":"t"}]';
 		let jsonData = JSON.parse(data);
 		jsonData = jsonData[0];
 		console.log(jsonData);*/
+		this.loadRooms();
+	}
+	
+	
+	loadRooms() {
 		this.jsonp.request("https://cloud.movilcrm.com/motel/backend/motel_rest.php?task=verPuertas&callback=JSONP_CALLBACK")
 		.subscribe(response => {
 			var res = response['_body'].verPuertas;
 			//console.log(Object.keys(res));
-			var keys = Object.keys(res)
+			var keys = Object.keys(res);
+			this.rooms = [];
 			for(var i=0;i< keys.length; i++) {
 				//console.log(res[keys[i]);
 				this.rooms.push({'id' : keys[i], 'status' : res[keys[i]]});
 			}
 			console.log(this.rooms);
+			let time = this.timeout * 1000;
+			let me = this;
+			setTimeout(function(){
+				me.loadRooms();
+			}, time);
 		});
+		
 	}
 	
 	checkButton(id) {
@@ -37,6 +51,10 @@ export class Dashboard1Component {
 	
 	favouriteButton(id) {
 		alert("Favourite Button Room " + id);
+	}
+	
+	fourthButton(id) {
+		alert("Fourth Button Room " + id);
 	}
 
 }
